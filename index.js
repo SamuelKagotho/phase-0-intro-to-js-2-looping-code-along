c121148374c48d4b7eeeb367c7f6fbb057550991
samuelkagotho <samuelkagotho@gmail.com>
2024-10-02 15:37:55 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards( name, item ) {
+    let thankYouCards = []
+    for ( let i = 0; i < name.length; i++ ) {
+      thankYouCards.push( `Thank you, ${name[i]}, for the wonderful ${item} gift!` )
+    }
+    return thankYouCards
+  }
+  
+  function countDown( startNum ) {
+    while ( startNum > 0 ) {
+      console.log( startNum );
+      startNum -= 1;
+    }
+    console.log( startNum );
+  }
